2087d9d9bae3c554a82e36ab8018c822e993c469
EnrValentina <enrvalentina0@gmail.com>
2025-10-17 10:04:44 -0500

    Initial commit with README and bashing_boxes.sh

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 boxes=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander "Shopping Bag")
+for item in "${boxes[@]}"; do
+    echo "$item"
+done
