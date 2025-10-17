dbea090473451e07116403bea9d8950a2059a90b
EnrValentina <enrvalentina0@gmail.com>
2025-10-17 10:37:22 -0500

    Adding code to .sh file. I added looping so it lists all the items in the array and prints them, and then i added a while loop to add a menu option echoeing the list  and then i just added the firts 2 options that are printing the full list of arrays and then exiting the menu

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-boxes=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander "Shopping Bag")
+
+#the things in the array are in one box
+boxes=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander" "Shopping Bag") 
+
+#looping thru every array and printing the items
 for item in "${boxes[@]}"; do
     echo "$item"
+done
+
+#while loop so wwe can pick what to do from a menu
+while true; do
+    echo ""
+    echo "-- Bashing Boxes Menu --"
+    echo "1) Print list"
+    echo "2) Print item at X position in list"
+    echo "3) Add item to list"
+    echo "4) Remove last item from list"
+    echo "5) Remove item- from X position"
+    echo "6) Exit"
+    read -p "Select an option: " choice
+
+    if [ "$choice" -eq 1 ]; then
+        for item in "${boxes[@]}"; do
+            echo "$item"
+        done
+    elif [ "$choice" -eq 2 ]; then
+        echo "Bye!"
+        break
+    else
+        echo "Invalid choice"
+    fi
 done
