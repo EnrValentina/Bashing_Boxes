40dd87184057b0370e8dc60e6e140ad5227ef552
EnrValentina <enrvalentina0@gmail.com>
2025-11-06 10:23:08 -0600

    logic fixes

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#the things in the array are in one box
-boxes=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander" "Shopping Bag") 
+# The things in the array are in one box
+boxes=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander" "Shopping Bag")
 
-#looping thru every array and printing the items
+# Loop through every item in the array and print them
 for item in "${boxes[@]}"; do
     echo "$item"
 done
 
-#while loop so we can pick what to do from a menu
+# While loop for interactive menu
 while true; do
     echo ""
     echo "-- Bashing Boxes Menu --"
@@ -16,22 +16,20 @@ while true; do
     echo "2) Print item at X position in list"
     echo "3) Add item to list"
     echo "4) Remove last item from list"
-    echo "5) Remove item- from X position"
+    echo "5) Remove item from X position"
     echo "6) Exit"
-    echo "7) Saving current box to a file"
-    echo "8) Loading a previously saved box"
-    echo "9) Listing existing saved boxes"
-    echo "10) Deleting a saved box"
+    echo "7) Save current box to a file"
+    echo "8) Load a previously saved box"
     read -p "Select an option: " choice
 
-#option to print out the whole list of arrays
+    # Option 1: Print all items
     if [ "$choice" -eq 1 ]; then
         echo "The full list is:"
         for item in "${boxes[@]}"; do
             echo "$item"
         done
 
-#option to print an item at X position in the list
+    # Option 2: Print item at X position
     elif [ "$choice" -eq 2 ]; then
         read -p "Enter position (1-${#boxes[@]}): " pos
         index=$((pos - 1))
@@ -40,79 +38,72 @@ while true; do
         else
             echo "Invalid position!"
         fi
-#option to add an item to the list
+
+    # Option 3: Add new item
     elif [ "$choice" -eq 3 ]; then
         read -p "What item would you like to add?: " new_item
         boxes+=("$new_item")
         echo "\"$new_item\" added."
 
-#option to remove last item to the list
+    # Option 4: Remove last item
     elif [ "$choice" -eq 4 ]; then
         unset 'boxes[-1]'
-        echo "Last item removed"
+        echo "Last item removed."
 
-
-#option to remove item- from X position
+    # Option 5: Remove item from X position
     elif [ "$choice" -eq 5 ]; then
         read -p "Enter position to remove (1-${#boxes[@]}): " pos
         index=$((pos - 1))
         if [ "$index" -ge 0 ] && [ "$index" -lt "${#boxes[@]}" ]; then
             echo "Removing ${boxes[$index]}..."
             unset "boxes[$index]"
-            boxes=("${boxes[@]}") #rebuild array
+            boxes=("${boxes[@]}") # rebuild array to remove empty slot
         else
             echo "Invalid position!"
         fi
 
-#option to exit the list and the menu
+    # Option 6: Exit
     elif [ "$choice" -eq 6 ]; then
         echo "Bye!"
         break
-    
 
-#saved boxes options
-
-
-#option to save current box to a file
+    # Option 7: Save current box
     elif [ "$choice" -eq 7 ]; then
         if [ ! -d "data" ]; then
             mkdir data
             echo "Created data/ directory."
         fi
-
         read -p "Enter name for saved box: " name_of_file
         declare -p boxes > "data/${name_of_file}.sh"
         echo "Box saved as data/${name_of_file}.sh"
 
-    fi
-
-#option to load a previously saved box
+    # Option 8: Load saved box
     elif [ "$choice" -eq 8 ]; then
-    if [ ! -d "data" ]; then
-        echo "No saved boxes (no data/ folder)"
-    else
-        echo "Saved Boxes:"
-        shopt -s nullglob #makes sure the files array is empty instead of containing a literal data/*.sh if there are no .sh files
-        files=(data/*.sh)
-        if [ ${#files[@]} -eq 0 ]; then
-            echo "No saved boxes yet"
+        if [ ! -d "data" ]; then
+            echo "No saved boxes (no data/ folder)"
         else
-            #list saved
-            for file in "${files[@]}"; do
-                echo "- ${file#data/}"
-            done
-
-            #ask which to load
-            read -p "Enter name of file you want to see (wo .sh)" load_name
-            filepath="data/${load_name}.sh"
-            #check if real
-            if [ -f "$filepath" ]; then
-                source "$filepath"
-                echo "Box '$load_name' loaded"
+            echo "Saved Boxes:"
+            shopt -s nullglob
+            files=(data/*.sh)
+            if [ ${#files[@]} -eq 0 ]; then
+                echo "No saved boxes yet."
             else
-                echo "Error file not found"
+                for file in "${files[@]}"; do
+                    echo "- ${file#data/}"
+                done
+                read -p "Enter name of file to load (without .sh): " load_name
+                filepath="data/${load_name}.sh"
+                if [ -f "$filepath" ]; then
+                    source "$filepath"
+                    echo "Box '$load_name' loaded."
+                else
+                    echo "Error: file not found."
+                fi
             fi
         fi
-    fi
+
+    
+
 done
+
 exit 0
--- sample_script_mrwarren.sh
+++ sample_script_mrwarren.sh
@@ -1,24 +1,40 @@
 #!/bin/bash 
 
-my_array_of_things=("thing1" "thing2" "thing3")
+my_array_of_things=("Key" "Rolling Pin" "Notebook" "Birdhouse" "Dishwasher" "Toothbrush" "Chalkboard" "Shoelaces" "Colander" "Shopping Bag")
 
 user_input="Error: could not retrieve user selection."
 
 display_main_menu(){
 	clear
 	echo -e "
-	| Main Menu |
-	_____________
+	| My Array of Things
+	____________________________
 	|
-	| 1) print all objects
+	| 1) Key
+	| 2) Rolling Pin
+	| 3) Notebook
+	| 4) Birdhouse
+	| 5) Dishwasher
+	| 6) Toothbrush
+	| 7) Chalkboard
+	| 8) Shoelaces
+	| 9) Colander
+	| 10) Shopping Bag
+	|
+	-----------------------------"
+	echo -e "
+	| Main Menu |
+	______________________________________
+	|
+	| 1) Print all objects
 	| 2) Print item at X position in list
-	| 3)
-	| 4)
-	| 5)
-	| 6)
-	| 7) save or load objects
-	| 8) exit app
-	-------------
+	| 3) Add item to list
+	| 4) Remove last item from list
+	| 5) Remove item from X position
+	| 6) Save or Load Boxes Menu
+	| 7) Exit app
+	|
+	--------------------------------------
 	"
 	get_user_input
 	check_main_menu_selection
@@ -28,51 +44,85 @@ display_save_load_menu(){
 	clear
 	echo -e "
 	| Save & Load Menu |
-	_____________
+	___________________________
 	|
 	| 1) Save current box
 	| 2) Load a Box
 	| 3) Go back to main menu
 	| 4) exit app
-	-------------
+	|
+	---------------------------
 	"
 	get_user_input
 	check_save_menu_selection
 }
 
 print_all_items_in_box(){
-	echo ${my_array_of_things[@]}
+	echo "Current box contents:"
+    echo "${my_array_of_things[@]}"
+    echo "$item"
 }
 
 print_item_at_x_position(){
-	:
+	read -p "Enter position number: " pos
+	echo "${my_array_of_things[$((pos-1))]}"
+	
 }
 
-remove_item_at_x_position(){
-	:
+add_item_to_list(){
+	read -p "What item would you like to add?: " new_item
+        my_array_of_things+=("$new_item")
+        echo "$new_item added."    
+}
+
+remove_last_item(){
+	unset 'my_array_of_things[${#my_array_of_things[@]}-1]'
+    echo "Last item removed."
+}
+
+remove_item_from_x_position(){
+	read -p "Enter position to remove: " pos
+	unset 'my_array_of_things[$((pos-1))]'
+	my_array_of_things=("${my_array_of_things[@]}") # reindex
+	echo "Item removed."
 }
 
 save_current_box(){
-	:
+	read -p "Enter name of file to save: " filename
+	printf "%s\n" "${my_array_of_things[@]}" > "$filename"
+	echo "Box saved as $filename"
 }
 
 load_box_from_data(){
-	:
+	read -p "Enter file you want to load: " filename
+	mapfile -t my_array_of_things < "$filename" 2>/dev/null
+	echo -e "\nBox "$filename" loaded"
+	echo "-----------------------------"
+	for item in "${my_array_of_things[@]}"; do
+		echo " - $item"
+	done
+	echo "-----------------------------"
 }
 
 get_user_input(){
-	read -p "Enter selection: " user_input
+	read -p "Enter selection from Main Menu: " user_input
 }
 
 check_main_menu_selection(){
 	case $user_input in
 		1) print_all_items_in_box
 			;;
-		2) echo "option two from MAIN menu"
+		2) print_item_at_x_position
 			;;
-		3) display_save_load_menu
+		3) add_item_to_list
 			;;
-		Leave) exit
+		4) remove_last_item
+			;;
+		5) remove_item_from_x_position
+			;;
+		6) display_save_load_menu
+			;;
+		7) exit
 			;;
 		*) display_main_menu
 			;;
@@ -81,18 +131,17 @@ check_main_menu_selection(){
 
 check_save_menu_selection(){
 		case $user_input in
-		1) print_all_items_in_box
+		1) save_current_box
 			;;
-		2) echo "the other menus option 2"
+		2) load_box_from_data
 			;;
 		3) display_main_menu
 			;;
-		A|a) exit
+		4) exit
 			;;
 		*) display_save_load_menu
 			;;
 	esac
 }
-
 
 display_main_menu
